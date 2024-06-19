b02f74ac197a8ea0e7cc77447f2dd31648a00a72
Dushan Senadheera <dcsenadheera777@gmail.com>
2024-06-19 23:07:42 +0530

    update admin dashboard

--- client/src/layout/AdminLayout/Dashboard.jsx
+++ client/src/layout/AdminLayout/Dashboard.jsx
@@ -4,13 +4,24 @@ import { NavLink, Outlet } from 'react-router-dom';
 export default function Dashboard() {
   return (
     <div className={styles.dashboardLayout}>
-      <ul className={styles.dashboardNav}>
-        <NavLink to={'/admin/dashboard'}>asdsad</NavLink>
-        <NavLink to={'/admin/dashboard'}>asdsad</NavLink>
-        <NavLink to={'/admin/dashboard'}>asdsad</NavLink>
-      </ul>
+      <nav className={styles.dashboardNav}>
+        <h3>Admin Dashboard</h3>
+        <ul>
+          <NavLink to={'/admin/dashboard'}>Username</NavLink>
+          <NavLink to={'/admin/dashboard'}>ICON</NavLink>
+        </ul>
+      </nav>
       <div className={styles.dashboardContent}>
-        <Outlet />
+        <ul className={styles.dashboardNavlinks}>
+          <NavLink to={'/admin/dashboard'}>Overview</NavLink>
+          <NavLink to={'/admin/dashboard'}>Guest Log</NavLink>
+          <NavLink to={'/admin/dashboard'}>Room Management</NavLink>
+          <NavLink to={'/admin/dashboard'}>Settings</NavLink>
+          <NavLink className={styles.logout} to={'/admin/dashboard'}>Logout</NavLink>
+        </ul>
+        <div className={styles.dashboardContent}>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
